13ef62732829584c408cf3a504ff739d83b22785
tordvb10 <tordvb10.kodehode@gmail.com>
2023-12-18 15:07:21 +0100

    ferdig for idag

--- README.md
+++ README.md
@@ -4,3 +4,7 @@ solo project about jupiter
 Eneste finjustering på html er meta-informasjon, og background-image og tekst på introduksjon fix til absolute.
 
 implementer med javascript for bytting av engelsk og norsk tekst.
+
+- ferdig for boldredigering i knapp
+- object for bytte av tekst - gjøre
+- skrive all tekst både på engelsk og norsk
--- index.js
+++ index.js
@@ -1,4 +1,42 @@
 const knapp = document.getElementById("englishnorsk");
+let language = "norsk";
+let english = document.querySelector("#englishnorsk .english");
+let norsk = document.querySelector("#englishnorsk .norsk");
+console.log(knapp);
+console.log(english);
+console.log(document.getElementsByClassName("english"));
 addEventListener("click", function () {
   console.log("knappen er trykket!");
+  if (language === "norsk") {
+    console.log("Konverterer nettsiden til Engelsk.");
+    //Converter nettside start
+    english.style.fontWeight = "400";
+    norsk.style.fontWeight = "900";
+    //Converter nettside slutt
+    language = "english";
+  } else if (language === "english") {
+    console.log("Converting the webpage to Norwegian.");
+    //Converting webpage start
+    english.style.fontWeight = "900";
+    norsk.style.fontWeight = "400";
+    //Converting webpage end
+    language = "norsk";
+  }
 });
+
+//const arraysss  {
+//  {
+//    let queryselector: ""
+//  let norsken: ""
+//let enelsken: ""
+//},
+
+//{
+//  let queryselector: ""
+//let norsken: ""
+//let enelsken: ""
+//}
+//}
+//for (i<0, arraysss.length,i++){
+//  arraysss[i].queryselector
+//}
