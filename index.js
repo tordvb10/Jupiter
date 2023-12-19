685d22ac7cb9fa9256602349fad4f1a6e5c5503c
tordvb10 <tordvb10.kodehode@gmail.com>
2023-12-19 09:09:30 +0100

    glemte pushe index.js igår.

--- index.js
+++ index.js
@@ -11,6 +11,7 @@ addEventListener("click", function () {
     console.log("Konverterer nettsiden til Engelsk.");
     //Converter nettside start
     english.style.fontWeight = "400";
+
     norsk.style.fontWeight = "900";
     //Converter nettside slutt
     language = "english";
@@ -18,6 +19,7 @@ addEventListener("click", function () {
     console.log("Converting the webpage to Norwegian.");
     //Converting webpage start
     english.style.fontWeight = "900";
+
     norsk.style.fontWeight = "400";
     //Converting webpage end
     language = "norsk";
